86b7618e61f301427add2eb96626bb2e5d8ceab8
Lucus Riccioni <lucusroni@gmail.com>
2022-03-23 03:35:54 +0000

    Commit

--- express_server.js
+++ express_server.js
@@ -5,13 +5,12 @@ app.set("view engine", "ejs");
 const cookieParser = require('cookie-parser')
 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}), cookieParser());
 
 
 function generateRandomString() {
 return Math.random().toString(36).slice(-6)
 }
-
 
 const urlDatabase = {
   "b2xVn2": "www.lighthouselabs.ca",     //try to make sure data is stored in the same format (HTTP://)
@@ -19,19 +18,29 @@ const urlDatabase = {
 };
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
-app.post("/urls", (req, res) => {
+app.post("/urls", (req, res) => {      
   const randomURL = generateRandomString();
   urlDatabase[randomURL] = req.body.longURL;
   res.redirect('http://localhost:8080/urls/' + randomURL)
 });
 
-app.post("/login", (req, res) => {                          //login
-  console.log("test%%%", req.body)
+app.post("/login", (req, res) => {                               //login
+  let username = req.body.username;
+  res.cookie("username", username)
+  res.redirect('/urls');
+});
 
+app.post("/logout", (req, res) => {  
+  let username = req.body.username                             //logout
+  res.clearCookie("username", username)
+  res.redirect('/urls');
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {                //deletes URL
@@ -39,8 +48,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {                //deletes URL
   res.redirect('/urls');
 });
 
-app.post("/urls/:newURL", (req, res) => {                        //edits URL
-  // let newURL = req.body.newURL
+app.post("/urls/:newURL", (req, res) => {                         //edits URL
   let shortURL = req.params.newURL
   urlDatabase[shortURL] = req.body.newURL
   res.redirect('/urls');
@@ -48,7 +56,7 @@ app.post("/urls/:newURL", (req, res) => {                        //edits URL
 
 app.get("/u/:shortURL", (req, res) => {
     const longURL = urlDatabase[req.params.shortURL]
-    res.redirect('http://' + longURL);      //includes http:// - this won't work if doubled
+    res.redirect('http://' + longURL);                           //includes http:// - this won't work if link already includes it
 });
 
 app.get("/", (req, res) => {
@@ -64,11 +72,20 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { 
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = { 
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
